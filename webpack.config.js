46739121be4ede78ccb3cb307da051c86c383cbc
holly-fontaine <hollybf@proton.me>
2026-06-19 00:54:47 +0100

    finished

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,26 @@
 const path = require('path');
+const webpack = require('webpack');
 
-module.exports = {};
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('development')
+        })
+    ]
+};
